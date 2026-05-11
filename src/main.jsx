8e29867297902995cf1d447885088e364fd967a5
spnaumowicz <stefan_naumowicz837@sunyempire.edu>
2026-05-11 14:49:02 -0400

    Update legal contact email

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import AdminPage from './admin/AdminPage';
+import { PrivacyPolicyPage, TermsPage } from './pages/LegalPages';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,6 +11,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/admin" element={<AdminPage />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+        <Route path="/terms" element={<TermsPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
